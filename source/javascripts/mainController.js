0fe3c5c80f60772cbd280c1a98a5d1ba246005f4
timsvoice <timsethvoice@gmail.com>
2014-08-04 23:22:44 -0400

    connected JSON data

--- source/javascripts/mainController.js
+++ source/javascripts/mainController.js
@@ -1,16 +1,35 @@
-app.controller('mainController', ['$scope', 'vendorsAPI',
-  function($scope, vendorsAPI) {
+app.controller('mainController', ['$scope', 'vendorsAPI', '$http',
+  function($scope, vendorsAPI, $http) {
+    var url = "data/vendors.json";
+    
+    $scope.vendorList = [];
+    $scope.categories = [];
+    $scope.states = [];
 
-  $scope.vendors = [];
-
-  getData();
-
-    function getData() {
-      vendorsAPI.getData()
-      .success(function (data) {
-        $scope.vendors.push(data);
-        console.log($scope.vendors);
+    $http.get(url, {
+      cache: true
+    }).
+    success(function(data){
+      $scope.vendorList = data;
+      console.log($scope.vendorList);
+      angular.forEach($scope.vendorList, function(value, index){
+        
+        // create categories for catList model
+        angular.forEach(value.categories, function(categoryList, index){
+          // only add categories if they don't already exist
+          var exists = false;
+          // loop through categories
+          angular.forEach($scope.categories, function(category, index){
+            if (category == categoryList) {
+              exists = true;
+            }
+          });
+          // if category is unique, add to categories
+          if (exists === false) {
+            $scope.categories.push(categoryList);
+            console.log($scope.categories);
+          }
+        });
       });
-    }
-
+    });
 }]);
--- source/javascripts/services.js
+++ source/javascripts/services.js
@@ -1,17 +1,3 @@
 app.factory('vendorsAPI', function ($http) {
   
-  var url = "data/vendors.json";
-  
-  var vendorsAPI = {};
-
-  vendorsAPI.getData = function() {
-    return $http.get(url).success(function(data){
-      console.log("success");
-    }).error(function(error) {
-      console.log("fail");
-    });
-  };
-  
-  return vendorsAPI;
-
 });
